509b3eb5d8b0a1c9aff8e567f4651c0f666392dc
granth2023 <grantharris38@gmail.com>
2023-07-11 16:56:11 -0400

    host

--- aipoll_server/settings.py
+++ aipoll_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@^9qvg)im5!8sjn&*x2zhqpou6&&@3@hcf5u8-&g_-igo7ys!+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
