94515d5425d895dcc1636b0d9fdd244515d53099
granth2023 <grantharris38@gmail.com>
2023-07-12 09:25:42 -0400

    new resource

--- aipoll_server/settings.py
+++ aipoll_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@^9qvg)im5!8sjn&*x2zhqpou6&&@3@hcf5u8-&g_-igo7ys!+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -147,6 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
